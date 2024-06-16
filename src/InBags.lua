ca7e57ebd87c30057e533f6f3f2373a9b25d5f91
opussf <github@zz9-za.com>
2024-06-16 03:08:59 -0700

    Some Work

--- src/InBags.lua
+++ src/InBags.lua
@@ -1,7 +1,97 @@
-InBag_SLUG, InBag = ...
-InBag_MSG_ADDONNAME = GetAddOnMetadata( InBag_SLUG, "Title" )
-InBag_MSG_VERSION   = GetAddOnMetadata( InBag_SLUG, "Version" )
-InBag_MSG_AUTHOR    = GetAddOnMetadata( InBag_SLUG, "Author" )
+InBags_SLUG, InBags = ...
+InBags_MSG_ADDONNAME = GetAddOnMetadata( InBags_SLUG, "Title" )
+InBags_MSG_VERSION   = GetAddOnMetadata( InBags_SLUG, "Version" )
+InBags_MSG_AUTHOR    = GetAddOnMetadata( InBags_SLUG, "Author" )
 
+-- Colours
+COLOR_RED = "|cffff0000"
+COLOR_GREEN = "|cff00ff00"
+COLOR_BLUE = "|cff0000ff"
+COLOR_PURPLE = "|cff700090"
+COLOR_YELLOW = "|cffffff00"
+COLOR_ORANGE = "|cffff6d00"
+COLOR_GREY = "|cff808080"
+COLOR_GOLD = "|cffcfb52b"
+COLOR_NEON_BLUE = "|cff4d4dff"
+COLOR_END = "|r"
 
+InBags_data = {}
 
+function InBags.Print( msg, showName)
+	-- print to the chat frame
+	-- set showName to false to suppress the addon name printing
+	if (showName == nil) or (showName) then
+		msg = COLOR_PURPLE..InBags_MSG_ADDONNAME.."> "..COLOR_END..msg
+	end
+	DEFAULT_CHAT_FRAME:AddMessage( msg )
+end
+
+function InBags.OnLoad()
+	SLASH_INBAGS1 = "/INBAGS"
+	SlashCmdList["UNBAGS"] = function( msg ) InBags.Command( msg); end
+
+	InBags_Frame:RegisterEvent( "BANKFRAME_OPENED" )
+	InBags_Frame:RegisterEvent( "ADDON_LOADED" )
+	InBags_Frame:RegisterEvent( "VARIABLES_LOADED" )
+	InBags_Frame:RegisterEvent( "PLAYER_LEAVING_WORLD" )
+end
+function InBags.ADDON_LOADED()
+	InBags_Frame:UnregisterEvent( "ADDON_LOADED" )
+	InBags.realm = GetRealmName()
+	InBags.name = UnitName("player")
+end
+function InBags.VARIABLES_LOADED()
+	InBags_Frame:UnregisterEvent( "VARIABLES_LOADED" )
+	InBags_data[InBags.realm] = InBags_data[InBags.realm] or {}
+	InBags_data[InBags.realm][InBags.name] = InBags_data[InBags.realm][InBags.name] or {}
+	InBags.me = InBags_data[InBags.realm][InBags.name]
+end
+function InBags.BANKFRAME_OPENED()
+end
+function InBags.PLAYER_LEAVING_WORLD()
+end
+
+function InBags.AddItem( p1, p2 )
+	print( p1..":"..p2 )
+end
+
+function InBags.ParseCmd(msg)
+	if msg then
+		local i,c,f = strmatch(msg, "^(|c.*|r)%s*(%d*)%s*(%S*)$")
+		if i then  -- i is an item, c is a count or nil
+			return i, c..(f and " "..f)
+		else  -- Not a valid item link
+			msg = string.lower(msg)
+			local a,b,c = strfind(msg, "(%S+)")  --contiguous string of non-space characters
+			if a then
+				-- c is the matched string, strsub is everything after that, skipping the space
+				return c, strsub(msg, b+2)
+			else
+				return ""
+			end
+		end
+	end
+end
+function InBags.Command(msg)
+	local cmd, param = InBags.ParseCmd(msg)
+	-- print( cmd..":"..param )
+	local cmdFunc = InBags.commandList[cmd]
+	if cmdFunc then
+		cmdFunc.func(param)
+	elseif (cmd and cmd ~= "") then
+		InBags.AddItem( cmd, param )
+	else
+		InBags.PrintHelp()
+	end
+end
+function InBags.PrintHelp()
+	InBags.Print(InBags_MSG_ADDONNAME.." ("..InBags_MSG_VERSION..") by "..InBags_MSG_AUTHOR)
+	for cmd, info in pairs(InBags.commandList) do
+		InBags.Print(string.format("%s %s %s -> %s",
+			SLASH_InBags1, cmd, info.help[1], info.help[2]))
+	end
+end
+
+InBags.commandList = {
+
+}
--- test/test.lua
+++ test/test.lua
@@ -8,10 +8,50 @@ test.outFileName = "testOut.xml"
 ParseTOC( "../src/InBags.toc" )
 
 function test.before()
+	InBags_data = {}
+	InBags.me = nil
+	InBags.OnLoad()
+	InBags.ADDON_LOADED()
+	InBags.VARIABLES_LOADED()
 end
 function test.after()
 end
-function test.test_01()
+-- Structure
+------------------------------------------
+-- ADDON_LOADED, and VARIABLES_LOADED should set up missing structure
+function test.test_structure_setRealm()
+	assertTrue( InBags_data["testRealm"] )
 end
+function test.test_structure_setName()
+	assertTrue( InBags_data["testRealm"]["testPlayer"] )
+end
+function test.test_structure_setMe()
+	assertTrue( InBags.me )
+	assertEquals( InBags.me, InBags_data["testRealm"]["testPlayer"], "InBags.me should point to InBags_data for current player." )
+end
+-- Add Item
+------------------------------------------
+-- Add an item to manage
+function test.test_addItem_link()
+	InBags.Command( "|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0|h[Broken Fang]|h|r" )
+end
+function test.test_addItem_text()
+	InBags.Command( "item:7073" )
+end
+function test.test_addItem_link_quanity()
+	InBags.Command( "|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0|h[Broken Fang]|h|r 10" )
+end
+function test.test_addItem_text_quanity()
+	InBags.Command( "item:7073 12" )
+end
+function test.test_addItem_link_quanity_flag()
+	InBags.Command( "|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0|h[Broken Fang]|h|r 14 once" )
+end
+function test.test_addItem_text_quanity_flag()
+	InBags.Command( "item:7073 16 once" )
+end
+
+
+
 
 test.run()
